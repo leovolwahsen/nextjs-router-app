b09d7abd31dd0c0be9e05bf89170e4b477196d23
Leo Volwahsen <volwahsenl@gmail.com>
2023-11-21 19:55:45 +0100

    React server component can now get data inside async home function to use DB prisma with await prisma.todo.findMany() to get all of the Inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
 import Link from "next/link";
+import { prisma } from "./db";
 
 
-export default function Home() {
+export default async function Home() {
+
+  const todos = await prisma.todo.findMany()
   return (
-  <>
-  <header>
-    <h1 className="tex-2xl">Todos</h1>
-    <Link href="/new">New</Link>
-  </header>
-  <ul></ul>
-  </>
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          href="/new"
+          className="border border-slate-300 text-slate-300 px-2 
+      py-2 rounded hover:bg-slate-700 focus-within:bg-slate-700
+      outline-none">New</Link>
+      </header>
+      <ul className="pl-4">{todos.map(todo => (
+        <li key={todo.id}>{todo.titel}</li>
+      ))}</ul>
+    </>
   )
 }
